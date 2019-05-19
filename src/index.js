ca6d8a69ef2fee69e4bfb3a05e644f280a54afc9
Elshad <safarov.elshad007@gmail.com>
2019-05-19 23:31:07 +0500

    Project structure changed

--- src/index.js
+++ src/index.js
@@ -1,37 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-const TodoList = () => {
-    return (
-        <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
-        </ul>   
-    );
-}
-
-const AppHeader = () => {
-    return (
-        <h1>My Todo List</h1>    
-    );
-}
-
-const SearchPanel = () => {
-    return (
-        <input placeholder="search" />
-    );
-}
-
-const App = () => {
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
-        </div>  
-    );
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
