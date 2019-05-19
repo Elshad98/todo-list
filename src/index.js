968c6e9c13328a5f26b22ba378b1d0e2d5e451a7
Elshad <safarov.elshad007@gmail.com>
2019-05-19 22:46:00 +0500

    Splitting an application into components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>   
+    );
+}
+
+const AppHeader = () => {
+    return (
+        <h1>My Todo List</h1>    
+    );
+}
+
+const SearchPanel = () => {
+    return (
+        <input placeholder="search" />
+    );
+}
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>  
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
