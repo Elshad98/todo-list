3d40a3b8c9ffc066cb71153d178b5edbe0a34d42
Elshad <safarov.elshad007@gmail.com>
2019-05-20 21:01:53 +0500

    Added handleClick method in TodoListItem class

--- src/components/todo-list-item/TodoListItem.js
+++ src/components/todo-list-item/TodoListItem.js
@@ -2,17 +2,33 @@ import React from 'react';
 import './TodoListItem.css';
 
 class TodoListItem extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            done: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            done: !this.state.done
+        });
+    }
+
     render() {
         const { label, important = false } = this.props;
-
+        const done = this.state.done;
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
 
         return (
-            <span className="todo-list-item">
+            <span className={`todo-list-item ${done ? 'done' : ''}`}>
                 <span
+                    onClick={this.handleClick}
                     className="todo-list-item-label"
                     style={style}>
                     {label}
