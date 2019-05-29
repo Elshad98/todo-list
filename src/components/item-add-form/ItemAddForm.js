e64f728bbc26edc6683e793f113bdccb278e615c
Elshad <safarov.elshad007@gmail.com>
2019-05-29 17:07:56 +0500

    Added handleChange and handleSubmit methods in ItemAddForm class

--- src/components/item-add-form/ItemAddForm.js
+++ src/components/item-add-form/ItemAddForm.js
@@ -2,13 +2,35 @@ import React from 'react';
 import './ItemAddForm.css';
 
 class ItemAddForm extends React.Component {
+	constructor(){
+		super();
+
+		this.state = {
+			text: ''
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(evt){
+		this.setState({
+			text: evt.target.value
+		});
+	}
+
+	handleSubmit(evt){
+		evt.preventDefault();
+		
+	}
+
 	render(){
 		return (
-            <div className="item-add-Form">
-				<button className="btn btn-outline-secondary">
+			<form onSubmit={this.handleSubmit} className="item-add-form">
+				<input type="text" value={this.state.text} onChange={this.handleChange} />
+				<button type="submit" className="btn btn-outline-secondary">
 					Add Item
 				</button>
-			</div>
+			</form>
         );
 	}
 }
