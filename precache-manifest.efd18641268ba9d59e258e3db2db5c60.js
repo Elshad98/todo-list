446328defaf5c88286f79fc30fc4b216f23ddfa6
Elshad98 <safarov.elshad007@gmail.com>
2021-01-15 19:39:23 +0500

    Updates

---
+++ precache-manifest.efd18641268ba9d59e258e3db2db5c60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2c06a156d2d3afde34897cd0908a8a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "5266771632fab0bddf14",
+    "url": "/todo-list/static/css/main.66d8b4c4.chunk.css"
+  },
+  {
+    "revision": "8a57eb1954f629ca12b7",
+    "url": "/todo-list/static/js/2.1b2f06e7.chunk.js"
+  },
+  {
+    "revision": "5266771632fab0bddf14",
+    "url": "/todo-list/static/js/main.bddb869a.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
